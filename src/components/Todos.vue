3ae4287634307fe19c8edf7290adb6b523dafc91
weedskimhj <kimhj@weedscomm.com>
2021-05-12 17:17:06 +0900

    animation 안에 있는 속성이 적용됨. mode 겹치지 않게

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -6,14 +6,16 @@
       @keypress.enter="addTodo"
       placeholder="Add a new todo..."
     />
-    <div v-if="todos.length">
-      <ul>
-        <li v-for="todo in todos" :key="todo.id" @click="deleteTodo(todo.id)">
-          {{ todo.text }}
-        </li>
-      </ul>
-    </div>
-    <div v-else>Woohoo, nothing left todo!</div>
+    <transition name="switch" mode="out-in">
+      <div v-if="todos.length">
+        <transition-group tag="ul" name="list" appear>
+          <li v-for="todo in todos" :key="todo.id" @click="deleteTodo(todo.id)">
+            {{ todo.text }}
+          </li>
+        </transition-group>
+      </div>
+      <div v-else>Woohoo, nothing left todo!</div>
+    </transition>
   </div>
 </template>
 
@@ -78,5 +80,46 @@ export default {
   }
   .todos li:hover {
     cursor: pointer;
+  }
+
+  /* list transitions */
+  .list-enter-from {
+    opacity: 0;
+    transform: scale(0.6);
+  }
+  .list-enter-to {
+    opacity: 1;
+    transform: scale(1);
+  }
+  .list-enter-active {
+    transition: all 0.4s ease;
+  }
+
+  .list-leave-from {
+    opacity: 1;
+    transform: scale(1);
+  }
+  .list-leave-to {
+    opacity: 0;
+    transform: scale(0.6);
+  }
+  .list-leave-active {
+    transition: all 0.4s ease;
+    position: absolute;
+  }
+
+  .list-move {
+    transition: all 0.3s ease;
+  }
+
+  /* switch transitions */
+  .switch-enter-from,
+  .switch-leave-to {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  .switch-enter-active,
+  .switch-leave-active {
+    transition: all 0.5s ease;
   }
 </style>
