ccaedd9504ca3de2728b2dedd67c4ff3d1b0375d
weedskimhj <kimhj@weedscomm.com>
2021-05-12 18:06:11 +0900

    route animation

--- src/App.vue
+++ src/App.vue
@@ -29,4 +29,20 @@ body {
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+/* route transitions */
+.route-enter-from {
+  opacity: 0;
+  transform: translateX(100px);
+}
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+.route-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
+}
+.route-leave-active {
+  transition: all 0.3s ease-in;
+}
 </style>
